e638f953b21fe1fb077605db74e8e6a9389a9c99
pisayesiwsi <152765566+pisayesiwsi@users.noreply.github.com>
2025-06-10 05:14:52 -0400

    border rendering for progress shapes
    last commit before I add the other shapes hopefully
    
    Signed-off-by: pisayesiwsi <152765566+pisayesiwsi@users.noreply.github.com>

--- display/progressShape.lua
+++ display/progressShape.lua
@@ -13,18 +13,21 @@ local ProgressShape = Proto:extend({
 	scale = Vec2(1, 1), --- How much to stretch the ProgressShape to.
 	size = Vec2(150, 30), -- Width and Height of the shape.
 	angle = 0, --- Rotation Angle
-
 	direction = ProgressStyling.Direction.LeftRight, --- Where should Colour 1 and Colour 2 be rendered?
 	smooth = false, --- Smoothly transitions between colour 1 and 2 when rendering.
 	current =   0, --- Current value.
 	maximum = 100, --- Maximum value that can be to reach.
 	minimum =   0, --- Minimum value that can be to reach.
 	rounded = true, --- Maintains the value in a linear range (always round numbers, never decimals).
-	tints = { --- Contains the colours to render the background and progress.
-		{ 0, 0.5, 0, 1 }, -- Dark Green
-		{ 0, 1.0, 0, 1 }, -- Green
-	},
 })
+local tints = { --- Contains the colours to render the background and progress.
+	{ 0, 0.5, 0, 1 }, -- Dark Green
+	{ 0, 1.0, 0, 1 }, -- Green
+}
+local border = { --- Handles the border behind the main colours.
+	tint = { 1, 1, 1, 1 },
+	thickness = 3,
+}
 
 function ProgressShape:init(x, y, dir)
 	self.position = Vec2(x or self.position.x, y or self.position.y)
@@ -49,17 +52,24 @@ function ProgressShape:draw()
 	love.graphics.rotate(self.angle) -- Rotation
 	love.graphics.scale(self.scale.get()) -- Scale
 
-	love.graphics.setColor(self.tints[1] or _defaultTint[1]) -- Draw Background
+	if border.thickness > 0 and border.tint[4] > 0 then
+		love.graphics.setColor(border.tint or _white)
+		love.graphics.setLineWidth(border.thickness or 1)
+		love.graphics.rectangle("line", 0, 0, self.size.x, self.size.y)
+	end
+
+	love.graphics.setColor(tints[1] or _defaultTint[1]) -- Draw Background
 	love.graphics.rectangle("fill", 0, 0, self.size.x, self.size.y)
 
 	local sizeProg = (self.current - self.minimum) / (self.maximum - self.minimum) -- I think, i hope.
-	love.graphics.setColor(self.tints[2] or _defaultTint[2]) -- Draw Progress
+	love.graphics.setColor(tints[2] or _defaultTint[2]) -- Draw Progress
 	if self.direction == ProgressStyling.Direction.RightLeft then
 		sizeProg = 1 - sizeProg
 	end
 	love.graphics.rectangle("fill", 0, 0, self.size.x * sizeProg, self.size.y)
 
 	love.graphics.setColor(_white)
+	love.graphics.setLineWidth(1)
 	love.graphics.pop()
 end
 
@@ -113,7 +123,7 @@ end
 --- @param b 		How much Blue (range is: 0-255)
 --- @param a 		How much Alpha (range is: 0-255)
 function ProgressShape:setBackgroundTint(r, g, b, a)
-	self.tints[1] = { love.math.colorFromBytes(r or 0, g or 100, b or 0, a or 255) }
+	tints[1] = { love.math.colorFromBytes(r or 0, g or 100, b or 0, a or 255) }
 	return self
 end
 
@@ -123,7 +133,33 @@ end
 --- @param b 		How much Blue (range is: 0-255)
 --- @param a 		How much Alpha (range is: 0-255)
 function ProgressShape:setProgressTint(r, g, b, a)
-	self.tints[2] = { love.math.colorFromBytes(r or 0, g or 100, b or 0, a or 255) }
+	tints[2] = { love.math.colorFromBytes(r or 0, g or 100, b or 0, a or 255) }
+	return self
+end
+
+--- Sets the border's tint and thickness at the same time.
+--- @param tint 		Table with 4 values { r, g, b, a }
+--- @param thickness 	Thickness of the border, a low value is recommended.
+--- @see https://love2d.org/wiki/love.math.colorFromBytes to use RGB(0-255).
+function ProgressShape:setBorder(tint, thickness)
+	border.tint = tint or { 1, 1, 1, 1 }
+	border.thickness = thickness or 0
+	return self
+end
+
+--- Changes the render colour for the border.
+--- @param r 		How much Red (range is: 0-255)
+--- @param g 		How much Green (range is: 0-255)
+--- @param b 		How much Blue (range is: 0-255)
+--- @param a 		How much Alpha (range is: 0-255)
+function ProgressShape:setBorderTint(r, g, b, a)
+	border.tint = { love.math.colorFromBytes(r or 0, g or 100, b or 0, a or 255) }
+	return self
+end
+
+--- Changes how thick the border behind the background and fill tints should be.
+function ProgressShape:setBorderThickness(x)
+	border.thickness = x or 0
 	return self
 end
 
