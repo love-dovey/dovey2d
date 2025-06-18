fb7ba5bc5e7453ed6b35d1602abc33efe5c1bb7f
pisayesiwsi <152765566+pisayesiwsi@users.noreply.github.com>
2025-06-18 12:01:41 -0400

    string.trim

--- util/stringtools.lua
+++ util/stringtools.lua
@@ -26,7 +26,23 @@ function string.endswith(x, ending)
 	return string.sub(x, - #ending) == ending
 end
 
+--- Trims a string using a pattern, trims spaces by default.
+---
+--- Sort of serves as a shortcut to string.gsub() with default patterns.
+--- see [§6.4.1](https://www.lua.org/manual/5.4/manual.html#6.4.1)
+--- @param x string
+--- @param customPattern? string
+--- @param replacement? string
+function string.trim(x, customPattern, replacement)
+	local p = type(customPattern) == "string" and customPattern or "%s+"
+	local r = type(replacement) == "string" and replacement or ""
+	return string.gsub(x, p, r)
+end
+
 --- Puts elements of a string in a table depending on the delimiter.
+--- @param x string
+--- @param delimiter string
+--- @param trimSpaces boolean 		Trims spaces from the resulting table keys.
 --- ```lua
 --- local combo = string.split("500", "")
 --- print(string.table(combo)) -- table 0x... {
@@ -41,18 +57,20 @@ end
 ---		3 = "banana",
 ---	}
 --- ```
-function string.split(x, delimiter)
+function string.split(x, delimiter, trimSpaces)
 	if #x == 0 then return {} end
+	if trimSpaces == nil then trimSpaces = false end
 	local out = {}
 	if delimiter == "" then
 		-- insert every element.
 		for i = 1, #x do
-			out[i] = string.sub(x, i, i)
+			local character = string.sub(x, i, i)
+			out[i] = trimSpaces and string.trim(x)
 		end
 	else
 		-- insert only elements after the delimiter.
 		for character in string.gmatch((x .. delimiter), "(.-)" .. delimiter) do
-			table.insert(out, character)
+			table.insert(out, trimSpaces and string.trim(character) or character)
 		end
 	end
 	return out
